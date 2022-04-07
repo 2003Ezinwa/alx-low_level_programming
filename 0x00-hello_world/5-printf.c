29dc4f2717a58bb3a07a0544b339b97cc500ced9
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-07 09:15:24 -0700

    Printing in C with the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the phrase: with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly' non-zero if otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
