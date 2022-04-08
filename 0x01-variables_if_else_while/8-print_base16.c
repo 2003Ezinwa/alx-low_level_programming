a576f8c593f8f2447f39e304d5707fab96319f32
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-08 09:35:21 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/** main - base 16
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int n;
+	char b;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (b = 'a'; b <= 'f'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
