c7a9249de3899cac938122585eb290d30cd473f7
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-08 08:14:07 -0700

    1

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
 	}
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 	{
 		putchar(alphabet);
 	}
