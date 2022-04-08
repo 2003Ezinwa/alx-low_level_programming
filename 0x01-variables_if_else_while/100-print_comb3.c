34dfa77052a47a7e9aeb65ba8b6e3505be792dff
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-08 10:29:58 -0700

    1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
 /**
  * main - print possible combinations
  * of two digit numbers without repeating
- * two single digits and having the same digits 
+ * two single digits and having the same digits
  * combined alternately
  * Return: 0 if successful
  */
@@ -13,15 +13,15 @@ int main(void)
 	int i;
 	int j;
 
-	for(i = 48; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
-		for(j = 48; j < 58; j++)
+		for (j = 48; j < 58; j++)
 		{
 			if (i != j && i < j)
 			{
 				putchar(i);
 				putchar(j);
-				if(j == 57 && i == 56)
+				if (j == 57 && i == 56)
 				{
 					break;
 				}
