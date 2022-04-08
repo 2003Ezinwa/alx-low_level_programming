37569147b6171bf3ef80c5eefb0be0b205ce0030
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-08 10:04:58 -0700

    possible combinations of single digits without the char type variable

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - possible combinations
+ * of single digits
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
