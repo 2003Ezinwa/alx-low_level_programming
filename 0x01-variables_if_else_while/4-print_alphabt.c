7aec6b5f589832ab97142ebfb0db5e89102c1cf6
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-08 08:26:27 -0700

    1

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		if (alphabet != 'q' && != 'e')
+		if (alphabet != 'q' && alphabet != 'e')
 		{
 			putchar(alphabet);
 		}
