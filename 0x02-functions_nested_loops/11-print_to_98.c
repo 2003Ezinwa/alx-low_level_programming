0dea36ee7d15023fef2eb02a4dccf73542cacafa
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-12 16:01:02 -0700

    4th

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,13 +17,13 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			n++;
 		}
-		else if (n > 98)
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
 		{
-			while (n > 98)
-			{
-				printf("%d, ", n);
-				n--;
-			}
+			printf("%d, ", n);
+			n--;
 		}
 	}
 	printf("98\n");
