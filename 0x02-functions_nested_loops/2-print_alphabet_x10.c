0a67b0f0b10a17989367f2b92e65a81fdd0f6154
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-12 13:17:03 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,15 @@ void print_alphabet_x10(void)
 	char letter = 'a';
 	int i = 0;
 
-	for (i = 0; i <= 10; i++)
+	while (i < 10)
 	{
+		letter = 'a';
 		while (letter <= 'z')
 		{
 			_putchar(letter);
 			letter++;
 		}
+		i++;
 	}
 	_putchar('\n');
 }
