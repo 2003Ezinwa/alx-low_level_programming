467753673348950de63ce7b13588bc765e556bcf
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-12 12:28:49 -0700

    printing with user function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,10 +10,10 @@ int main(void)
 	char text[9] = "_putchar";
 	int i = 0;
 
-	for (i = 0; i < 8; i++);
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(text[i]);
 	}
-	_putchar('\n);
+	_putchar('\n');
 	return (0);
 }
