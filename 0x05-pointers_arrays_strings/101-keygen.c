d8be9d9faa526661f2ef10e5e3ec26bbf46f386e
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-20 16:06:33 -0700

    advanced task for pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate random passwords for
+ * the program 101-crackme
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int i = 0, j = 0;
+	time_t t;
+
+	srand((unsigned int) time (&t));
+	while (j < 2772)
+	{
+		i = rand() % 128;
+		if ((j + i) > 2772)
+		{
+			break;
+		}
+		j += i;
+		printf("%c", i);
+	}
+	printf("%c\n", (2772 - j));
+	return (0);
+}
