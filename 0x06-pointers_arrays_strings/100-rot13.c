c4bb2a30460aa692402f888577556ad4cc2527cd
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-24 14:53:31 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string
+ * Return: pointer to dest
+ */
+
+char *rot13(char *s)
+{
+	int counter = 0, b;
+
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (s[counter] != '\0')
+	{
+		for (b = 0; b < 52; b++)
+		{
+			if (s[ounter] == alpha[b])
+			{
+				s[counter] = rot13[b];
+				break;
+			}
+		}
+		counter++;
+	}
+	return (s);
+}
