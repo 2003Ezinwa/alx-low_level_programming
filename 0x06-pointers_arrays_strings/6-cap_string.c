bd001e71db2e86b5363f446eb8928c8a7c7fa226
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-24 14:00:02 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words
+ * in the string
+ * @str: string
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+	int one, two;
+	char g[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+
+	one = 0;
+	while (str[one] != '\0')
+	{
+		if (one == 0 && str[one] >= 97 && str[one] <= 122)
+		{
+			str[one] = str[one] - 32;
+		}
+		two = 0;
+		while (g[two] != '\0')
+		{
+			if (g[two] == str[one] && str[one + 1] >= 97 && tr [one + 1] <= 122)
+			{
+				str[one + 1] = str[one + 1] - 32;
+			}
+			two++;
+		}
+		one++;
+	}
+	return (str);
+}
