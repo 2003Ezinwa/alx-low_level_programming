634c4c2d30684ab9866bcf8c6b3ee60505cc2782
Ihedioha Ezinwa Ezinma <102328122+2003Ezinwa@users.noreply.github.com>
2022-04-25 01:23:59 +0100

    Add files via upload
    Task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,52 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * infinite_add - adds two numbers
- * @n1: number 1
- * @n2: number 2
- * @r: buffer for storing
- * @size_r: buffer size
- * Return: pointer
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer for storing.
+ * @size_r: buffer size:
+ * Return: pointer to dest.
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a1 = 0, a2 = 0, bd, xo, get1, get2, sum = 0;
+	int e1 = 0, e2 = 0, df, wu, sx1, sx2, add = 0;
 
-	while (*(n1 + a1) != '\0')
-		a1++;
-	while (*(n2 + a2) != '\0')
-		a2++;
-	if (a1 >= a2)
-		xo = a1;
+	while (*(n1 + e1) != '\0')
+		e1++;
+	while (*(n2 + e2) != '\0')
+		e2++;
+	if (e1 >= e2)
+		wu = e1;
 	else
-		xo = a2;
-	if (size_r <= xo + 1)
+		wu = e2;
+	if (size_r <= wu + 1)
 		return (0);
-	r[xo + 1] = '\0';
-	a1--, a2--, size_r--;
-	get1 = *(n1 + a1) - 48, get2 = *(n2 + a2) - 48;
-	while (xo >= 0)
-		bd = get1 + get2 + sum;
-		if (bd >= 10)
-			sum = bd / 10;
+	r[wu + 1] = '\0';
+	e1--, e2--, size_r--;
+	sx1 = *(n1 + e1) - 48, sx2 = *(n2 + e2) - 48;
+	while (wu >= 0)
+	{
+		df = sx1 + sx2 + add;
+		if (df >= 10)
+			add = df / 10;
 		else
-			sum = 0;
-		if (bd > 0)
-			*(r + xo) = (bd % 10) + 48;
+			add = 0;
+		if (df > 0)
+		*(r + wu) = (df % 10) + 48;
 		else
-			*(r + xo) = '0';
-		if (a1 > 0)
-			a1--, get1 = *(n1 + a1) - 48;
+			*(r + wu) = '0';
+		if (e1 > 0)
+			e1--, sx1 = *(n1 + e1) - 48;
 		else
-			get1 = 0;
-		if (a2 > 0)
-			a2--, get2 = *(n2 + a2) - 48;
+			sx1 = 0;
+		if (e2 > 0)
+			e2--, sx2 = *(n2 + e2) - 48;
 		else
-			get2 = 0;
-		xo--, size_r--;
+			sx2 = 0;
+		wu--, size_r--;
+	}
 	if (*(r) == '0')
 		return (r + 1);
 	else
