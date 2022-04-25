005fd6c59e45ee5ed3035b5009869f558f2af89f
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-24 17:21:01 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,16 +7,16 @@
  * @n2: number 2
  * @r: buffer for storing
  * @size_r: buffer size
- * Return: r, r + 1, 0i
+ * Return: pointer
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int a1 = 0, a2 = 0, bd, xo, get1, get2, sum = 0;
 
-	while (n1[a1] != '\0')
+	while (*(n1 + a1) != '\0')
 		a1++;
-	while (n2[a2] != '\0')
+	while (*(n2 + a2) != '\0')
 		a2++;
 	if (a1 >= a2)
 		xo = a1;
@@ -26,7 +26,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		return (0);
 	r[xo + 1] = '\0';
 	a1--, a2--, size_r--;
-	get1 = n1[a1] - 48, get2 = n2[a2] - 48;
+	get1 = *(n1 + a1) - 48, get2 = *(n2 + a2) - 48;
 	while (xo >= 0)
 		bd = get1 + get2 + sum;
 		if (bd >= 10)
@@ -34,15 +34,15 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		else
 			sum = 0;
 		if (bd > 0)
-			r[xo] = (bd % 10) + 48;
+			*(r + xo) = (bd % 10) + 48;
 		else
-			r[xo] = '0';
+			*(r + xo) = '0';
 		if (a1 > 0)
-			a1--, get1 = n1[a1] - 48;
+			a1--, get1 = *(n1 + a1) - 48;
 		else
 			get1 = 0;
 		if (a2 > 0)
-			a2--, get2 = n2[a2] - 48;
+			a2--, get2 = *(n2 + a2) - 48;
 		else
 			get2 = 0;
 		xo--, size_r--;
