676f9d628c3b471b03e9dace7da045a378d8752b
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-26 14:16:19 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strchr - return the first occurence
+ * of a character in a string if it exists
+ * else  return null
+ * @s: string
+ * @c: character
+ * Return: s if equal to c and null if s != c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{
+			return (s);
+		}
+		break;
+	}
+	if (*(s + i) == c)
+	{
+		return (s);
+	}
+	else
+	{
+		return (0);
+	}
+}
