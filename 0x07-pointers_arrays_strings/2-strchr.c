5c45d73f97ae94a0077b54a658fd55e647436070
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-26 14:28:11 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,15 +13,15 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; *s != '\0'; i++)
 	{
-		if (*(s + i) == c)
+		if (*s == c)
 		{
 			return (s);
 		}
 		s++;
 	}
-	if (*(s + i) == c)
+	if (*s == c)
 	{
 		return (s);
 	}
