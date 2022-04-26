671987d84c9b935fcd1fb505529f85b5acadf0ab
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-26 15:26:59 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h£"
+
+/**
+ * _strstr - check for a string in a string
+ * @haystack: string to look into
+ * @needle: string to look for
+ * Return: pointer to the beginning of
+ * the located substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *a = haystack, *b = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack != *needle)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (a);
+		}
+		needle = b;
+		a++;
+		haystack = a;
+	}
+	return (0);
+}	
