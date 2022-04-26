575580abcdfdc816ecf9fd5aaa2490e36f160d78
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-26 16:00:29 -0700

    task 6, but seemes thay made a mistake and named it 7-print_chessboard.c instead of 6-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chessboard
+ * @a: 2D array
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int first, second;
+
+	for (first = 0; first < 8; first++)
+	{
+		for (second = 0; second < 8; second++)
+		{
+			_putchar (*(*(first + a) + j));
+		}
+	_putchar('\n');
+	}
+}
