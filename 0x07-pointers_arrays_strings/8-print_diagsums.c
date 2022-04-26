356696092015b2ee31b3d180742dec653f335f87
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-26 16:19:25 -0700

    task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print the sum of the elements
+ * of each diagonal
+ * @a: array
+ * @size: size of the array
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int k, asum = 0; bsum = 0;
+
+	for (k = 0; k < size; k++)
+	{
+		asum += *(a + (size * k + k));
+		bsum += *(a + (size * k + size - 1 - k));
+	}
+	printf("%d, ", asum);
+	printf("%d\n", bsum);
+}
