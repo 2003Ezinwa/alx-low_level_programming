0c86530dbe8fc4ab82e7f8dac0014c514ae104d2
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-28 14:33:49 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return the length
+ * of a string
+ * @s: the string
+ * Return: length
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+	
+	if (*s == '\0')
+	{
+		return (n);
+	}
+	else
+	{
+		n++;
+		return (n + _strlen_recursion(s + 1));
+	}
+}
+
