ee4a7fa07d18731c8650e9324f91a50de438a3b1
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-04-28 14:06:06 -0700

    main.h file and task 0 and readme file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_recursion - print a string followed
+ * by a new line
+ * @s: string
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*(s) != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*(s) == '\0')
+	{
+		_putchar('\n');
+	}
+}
