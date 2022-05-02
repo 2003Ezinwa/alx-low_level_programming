9f3b0de568f0522122a07b2f967df159bb366b6f
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-05-02 16:29:18 -0700

    task 0

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,19 +1,1 @@
-#include "main.h"
 
-/**
- * main - print out putchar
- * Return: 0 if successful
- */
-
-int main(void)
-{
-	char text[9] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-	return (0);
-}
