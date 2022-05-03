ea469df4ffdd713aa518d810c1c89109541e2511
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-05-02 18:01:36 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: param counter
+ * @argv: param pointer
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	/*stdlib was included because of the "atio()" function*/
+	/* the funct is used to covert str to int*/
+	/* because the argv is an arrays of characters*/
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
