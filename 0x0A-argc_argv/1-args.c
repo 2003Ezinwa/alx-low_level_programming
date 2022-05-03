e8f615ef9d3fea5b7c140605e2316b4199a32dfd
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-05-02 17:48:38 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: param counter
+ * @argv: param pointer
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
