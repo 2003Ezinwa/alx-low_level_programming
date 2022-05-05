65717d8cfb5a06505ff95d837929d56cb53a1b91
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-05-05 15:58:01 -0700

    task 6

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,4 +6,5 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **mat, int height);
 char *argstostr(int ac, char **av);
+char **strtow(char *str);
 #endif /*MAIN_H*/
