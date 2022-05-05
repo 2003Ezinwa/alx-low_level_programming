6918715b22cf3a20ccec40524e3cfd385153b4d9
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-05-05 14:11:59 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - create a new memory address
+ * with a copy of the string
+ * @str: the string
+ * Return: the new str if successful
+ */
+
+char *_strdup(char *str)
+{
+	char *newstr;
+	unsigned int a;
+	int b;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	a = 0;
+	while (str != '\0')
+	{
+		a++;
+	}
+	newstr = malloc(sizeof(char) * (a + 1));
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
+	for (b = 0; b < a; b++)
+	{
+		newstr[b] = str[b];
+	}
+	newstr[b] = '\0';
+	return (newstr);
+}
