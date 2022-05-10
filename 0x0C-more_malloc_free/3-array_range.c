85de4138cc52b54f376d2faabd7308176371e544
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-05-09 17:58:14 -0700

    task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,15 +13,17 @@ int *array_range(int min, int max)
 	int *arr, i = 0, j = min;
 
 	if (min > max)
+	{
 		return (0);
-
+	}
 	arr = malloc((max - min + 1) * sizeof(int));
-
 	if (!arr)
+	{
 		return (0);
-
+	}
 	while (i <= max - min)
+	{
 		arr[i++] = j++;
-
+	}
 	return (arr);
 }
