f7ccb3242eb33fabd1d20b5fad6c89b7150d5c3e
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-05-09 17:46:11 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,9 +10,7 @@
 void *malloc_checked(unsigned int b)
 {
 	void *i = malloc(b);
-	/*size of parameter alreay added from funct call*/
-	/*Type cast not needed*/
-
+	
 	if (i == NULL)
 		exit(98);
 	return (i);
