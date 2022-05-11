eeba5a255d6d95c2c704acc2e9d305cf3a2c216e
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-05-11 23:10:10 +0100

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib>
+
+/**
+ * main - print name of file it was compiled from
+ * followed by newline
+ * Return: int
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	Return (0);
+}
