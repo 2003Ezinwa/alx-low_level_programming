cef3c924a5e9cb489fbbea62beda7b9d50e351d1
2003Ezinwa <ihediohaezinwa@gmail.com>
2022-05-17 01:37:48 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,12 +3,12 @@
 /**
  * sum_them_all - summ all the arguments
  * @n: argument 1
- * Return: int
+ * Return: sum
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list list;
+	va_list valist;
 	unsigned int a;
 	int sum = 0;
 
@@ -16,11 +16,11 @@ int sum_them_all(const unsigned int n, ...)
 	{
 		return (0);
 	}
-	va_start(list, n);
+	va_start(valist, n);
 	for (a = 0; a < n; a++)
 	{
-		sum += va_arg(list, int);
+		sum += va_arg(valist, int);
 	}
-	va_end(list);
+	va_end(valist);
 	return (sum);
 }
