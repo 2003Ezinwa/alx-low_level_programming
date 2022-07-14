96d49a13f8e4a48822ef69046123985f117dbeb1
Ihedioha Ezinwa Ezinma <102328122+2003Ezinwa@users.noreply.github.com>
2022-07-14 23:19:35 +0100

    task 0

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory from one string to another
+ * @dest: string to be copied to
+ * @src: string to copy
+ * @n: number of bytes to copr from src
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int count;
+
+	for (count = 0; count < n; count++)
+	{
+		dest[count] = src[count];
+	}
+	return (dest);
+}
---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search a string for any of
+ * a set of bytes
+ * @s: string 1 to be searched
+ * @accept: set of bytes to search for in string 1
+ * Return: s if successful, NULL if otherwise
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a, b, count = 0;
+
+	for (a = 0; *s != '\0'; a++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (*s == accept[b])
+			{
+				count++;
+				break;
+			}
+
+		}
+		if (count)
+		{
+			return (s);
+		}
+		else
+		{
+			s++;
+		}
+
+	}
+	return (0);
+}
